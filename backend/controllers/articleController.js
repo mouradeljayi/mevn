0da046d303156da5612f4466523597f39f6f613a
mouradeljayi <mouradeljayi98@gmail.com>
2022-06-18 13:48:36 +0100

    REST API for Articles

--- backend/controllers/articleController.js
+++ backend/controllers/articleController.js
@@ -1,35 +1,59 @@
 const asyncHandler = require('express-async-handler')
+const Article = require('../models/articleModel')
+
 
 // @desc Get Articles
 // @route GET /api/articles
 // @access Private
 const getArticles = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get articles'})
+    const articles = await Article.find()
+    res.status(200).json(articles)
 })
 
 // @desc Set Article
 // @route POST /api/articles
 // @access Private
 const setArticle = asyncHandler(async (req, res) => {
-    if(!req.body.text) {
+   /* if(!req.body.title) {
         res.status(400)
         throw new Error('Please add a text field')
-    }
-    res.status(200).json({message: 'Create article'})
+    }*/
+    const article = await Article.create({
+        title: req.body.title,
+        description: req.body.description,
+        city: req.body.city,
+        imageUrl: req.body.imageUrl,
+        user: req.user.id
+    })
+    res.status(200).json(article)
 })
 
 // @desc Update Article
 // @route PUT /api/articles/:id
 // @access Private
 const updateArticle = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update article ${req.params.id}`})
+    const article = await Article.findById(req.params.id)
+    if(!article) {
+        res.status(400)
+        throw new Error('Article not found')
+    }
+    const updatedArticle = await Article.findByIdAndUpdate(req.params.id, req.body, {
+        new : true,
+    })
+    res.status(200).json(updatedArticle)
 })
 
 // @desc Delete Article
 // @route DELETE /api/articles
 // @access Private
 const deleteArticle = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete article ${req.params.id}`})
+    const article = await Article.findById(req.params.id)
+    if(!article) {
+        res.status(400)
+        throw new Error('Article not found')
+    }
+    await article.remove()
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
---
+++ backend/models/articleModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const articleSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please add a title value']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description value']
+    },
+    city: {
+        type: String,
+        required: [true, 'Please add a city value']
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'Please add an image value']
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+      },
+}, 
+    {
+        timestamps: true,
+    }
+)
+
+module.exports = mongoose.model('Article', articleSchema)
